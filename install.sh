0e2778a3503944722de1f5a0459532762b42e743
John Zila <john@jzila.com>
2024-04-19 10:09:06 -0500

    install.sh: Update for alacritty and tmux

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+DOTFILES="$HOME/repos/dotfiles"
 
-ln -s $HOME/repos/dotfiles/.vim $HOME/.config/nvim
-ln -s $HOME/repos/dotfiles/vimrc $HOME/.vimrc
+ln -s $DOTFILES/.vim $HOME/.config/nvim
+ln -s $DOTFILES/vimrc $HOME/.vimrc
+
+# alacritty
+mkdir -p ~/.config/alacritty
+ln -s $DOTFILES/alacritty.yml ~/.config/alacritty
+
+# tmux
+ln -s $DOTFILES/tmux.conf ~/.tmux.conf
