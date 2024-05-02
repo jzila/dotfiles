fd5d1cf91dc72a64a787de6cc86d81372f653294
John Zila <john@jzila.com>
2024-05-02 11:35:28 -0500

    kitty: use kitty terminal instead

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ DOTFILES="$HOME/repos/dotfiles"
 ln -s $DOTFILES/.vim $HOME/.config/nvim
 ln -s $DOTFILES/vimrc $HOME/.vimrc
 
-# alacritty
-mkdir -p ~/.config/alacritty
+# alacritty & kitty
+mkdir -p ~/.config/{alacritty,kitty}
 ln -s $DOTFILES/alacritty.yml ~/.config/alacritty
+ln -s $DOTFILES/kitty.conf ~/.config/kitty
+ln -s $DOTFILES/kitty-current-theme.conf ~/.config/kitty/current-theme.conf
 
 # tmux
 ln -s $DOTFILES/tmux.conf ~/.tmux.conf
